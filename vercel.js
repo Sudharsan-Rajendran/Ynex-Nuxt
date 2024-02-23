a154df385babcc735835c9c01f44174b80f72442
sudharsan <sudharsan.r@inforios.com>
2024-02-23 13:56:36 +0530

    vercel

---
+++ vercel.js
@@ -0,0 +1,22 @@
+{
+    "headers": [
+      {
+        "source": "**/*.@(js|css)",
+        "headers": [
+          {
+            "key": "Content-Type",
+            "value": "text/plain"
+          }
+        ]
+      },
+      {
+        "source": "**/*.woff",
+        "headers": [
+          {
+            "key": "Content-Type",
+            "value": "font/woff"
+          }
+        ]
+      }
+    ]
+  }
